4806d185807e318a2b070ab04a0bacd00ec28b93
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2017-01-24 10:25:37 +0800

    update script

--- script/script.py
+++ script/script.py
@@ -2,10 +2,16 @@ import os
 import glob
 import os
 
-def crop(name):
-    os.system('convert photo/%s -crop 2830x1360+300+580 exports/%s' % (name, name))
+pwd = str(os.getcwd())
+print pwd
 
-for file in glob.glob("photo/*.jpeg"):
-    photo = file.split('/', 1)[1]
+def crop(name):
+    os.system('convert %s/script/photo/%s -crop 2830x1360+300+580 %s/source/images/%s' % (pwd, name, pwd, name))
+
+for file in glob.glob(str(pwd) + "/script/photo/*.jpeg"):
+    # file: /Users/amedeospagnolo/DATA/GIT/TINKLABS/flow_android/script/photo/image.jpeg
+    # photo: image.jpeg
+    temp = file.split('/')
+    photo = temp[len(temp) - 1]
     print('cropping %s' % (photo))
     crop(photo)
